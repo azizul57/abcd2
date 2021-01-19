f11866c3f0ca8f43858489d14716a84b49fe306c
Azizul Islam <61428975+azizul57@users.noreply.github.com>
2021-01-20 01:26:33 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,242 @@
+var za= document.querySelector('.app')
+function a(){
+    za.innerHTML='A FOR APPLE';
+}
+var zb= document.querySelector('.b')
+function b(){
+    zb.innerHTML='A FOR BALL';
+}
+var zc= document.querySelector('.c')
+function c(){
+    zc.innerHTML='A FOR CAT';
+}
+var zd= document.querySelector('.d')
+function d(){
+    zd.innerHTML='A FOR DALL';
+}
+var ze= document.querySelector('.e')
+function e(){
+    ze.innerHTML='A FOR EGG';
+}
+var zf= document.querySelector('.f')
+function f(){
+    zf.innerHTML='A FOR FAN';
+}
+var zg= document.querySelector('.g')
+function g(){
+    zg.innerHTML='A FOR GIRL';
+}
+var zh= document.querySelector('.h')
+function h(){
+    zh.innerHTML='A FOR HORSE';
+}
+var zi= document.querySelector('.i')
+function i(){
+    zi.innerHTML='A FOR INK';
+}
+var zj= document.querySelector('.j')
+function j(){
+    zj.innerHTML='A FOR JUG';
+}
+var zk= document.querySelector('.k')
+function k(){
+    zk.innerHTML='A FOR KITE';
+}
+var zl= document.querySelector('.l')
+function l(){
+    zl.innerHTML='A FOR LION';
+}
+var zm= document.querySelector('.m')
+function m(){
+    zm.innerHTML='A FOR MANGO';
+}
+var zn= document.querySelector('.n')
+function n(){
+    zn.innerHTML='A FOR NET';
+}
+var zo= document.querySelector('.o')
+function o(){
+    zo.innerHTML='A FOR ORAGE';
+}
+var zp= document.querySelector('.p')
+function p(){
+    zp.innerHTML='A FOR PEN';
+}
+var zq= document.querySelector('.q')
+function q(){
+    zq.innerHTML='A FOR QUEEN';
+}
+var zr= document.querySelector('.r')
+function r(){
+    zr.innerHTML='A FOR RAINBOW';
+} 
+var zs= document.querySelector('.s')
+function s(){
+    zs.innerHTML='A FOR SUN';
+}
+var zt= document.querySelector('.t')
+function t(){
+    zt.innerHTML='A FOR TIGER';
+}
+var zu= document.querySelector('.u')
+function u(){
+    zu.innerHTML='A FOR UMBELLA';
+}
+var zv= document.querySelector('.v')
+function v(){
+    zv.innerHTML='A FOR VIROUS';
+}
+var zw= document.querySelector('.w')
+function w(){
+    zw.innerHTML='A FOR WINDOW';
+}
+var zx= document.querySelector('.x')
+function x(){
+    zx.innerHTML='A FOR XA-RAY';
+}
+var zy= document.querySelector('.y')
+function y(){
+    zy.innerHTML='A FOR YELLOW';
+}
+var zz= document.querySelector('.z')
+function z(){
+    zz.innerHTML='A FOR ZEBRA';
+}
+
+document.querySelectorAll('button')[0].addEventListener('click', play)
+ function play(){
+     var audio= new Audio('sound/a.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[1].addEventListener('click', b1)
+ function b1(){
+     var audio= new Audio('sound/b.mp3')
+     audio.play()
+}
+ document.querySelectorAll('button')[2].addEventListener('click', c2)
+function c2(){
+    var audio= new Audio('sound/c.mp3')
+    audio.play()
+ }
+ document.querySelectorAll('button')[3].addEventListener('click', d3)
+ function d3(){
+     var audio= new Audio('sound/d.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[4].addEventListener('click', e4)
+ function e4(){
+     var audio= new Audio('sound/e.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[5].addEventListener('click', f5)
+ function f5(){
+     var audio= new Audio('sound/f.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[6].addEventListener('click', g6)
+ function g6(){
+     var audio= new Audio('sound/g.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[7].addEventListener('click', h7)
+ function h7(){
+     var audio= new Audio('sound/h.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[8].addEventListener('click', i8)
+ function i8(){
+     var audio= new Audio('sound/i.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[9].addEventListener('click', j9)
+ function j9(){
+     var audio= new Audio('sound/j.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[10].addEventListener('click', k11)
+ function k11(){
+     var audio= new Audio('sound/k.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[11].addEventListener('click', l11)
+ function l11(){
+     var audio= new Audio('sound/l.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[12].addEventListener('click', m1)
+ function m1(){
+     var audio= new Audio('sound/m.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[13].addEventListener('click', n1)
+ function n1(){
+     var audio= new Audio('sound/n.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[14].addEventListener('click', o1)
+ function o1(){
+     var audio= new Audio('sound/o.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[15].addEventListener('click',p1)
+ function p1(){
+     var audio= new Audio('sound/p.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[16].addEventListener('click', q1)
+ function q1(){
+     var audio= new Audio('sound/q.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[17].addEventListener('click', r1)
+ function r1(){
+     var audio= new Audio('sound/r.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[18].addEventListener('click', s1)
+ function s1(){
+     var audio= new Audio('sound/s.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[19].addEventListener('click', t1)
+ function t1(){
+     var audio= new Audio('sound/t.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[20].addEventListener('click', u1)
+ function u1(){
+     var audio= new Audio('sound/u.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[21].addEventListener('click', v1)
+ function v1(){
+     var audio= new Audio('sound/v.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[22].addEventListener('click', w1)
+ function w1(){
+     var audio= new Audio('sound/w.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[23].addEventListener('click', x1)
+ function x1(){
+     var audio= new Audio('sound/x.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[24].addEventListener('click', y1)
+ function y1(){
+     var audio= new Audio('sound/y.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[25].addEventListener('click', z1)
+ function z1(){
+     var audio= new Audio('sound/z.mp3')
+     audio.play()
+ }
+ document.querySelectorAll('button')[26].addEventListener('click', tt)
+ function tt(){
+     var audio= new Audio('sound/thank.mp3')
+     audio.play()
+ }
+
+
